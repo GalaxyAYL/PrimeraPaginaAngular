01968e91b7d39f5590ef00496c90832eb66db300
GalaxyAYL <josealbertoayalazumba@gmail.com>
2022-11-07 08:20:43 -0500

    commit inicial

---
+++ src/components/component_b/component-b/component-b.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentBComponent } from './component-b/component-b.component';
+
+
+
+@NgModule({
+  declarations: [ComponentBComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentBModule { }
